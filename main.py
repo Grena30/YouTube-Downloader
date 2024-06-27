41cfd9fb58036fca289a6bb0cf9e379c99f09cb9
Grena30 <grenadinu@yahoo.com>
2024-06-27 15:39:27 +0300

    feat: add python script to download videos

---
+++ main.py
@@ -0,0 +1,37 @@
+from utils import download_channel, download_playlist, download_video
+import argparse
+from pathlib import Path
+
+DOWNLOAD_PATH: str = "Downloads\YouTube\\" # Change this to your download path
+
+def main() -> None:
+    parser = argparse.ArgumentParser(description="Download videos, audios, playlists, or channels from YouTube.")
+    parser.add_argument("url", metavar="\"URL\"", type=str, help="The URL of the video, playlist, or channel to download. Enclose the URL in quotes.")
+    parser.add_argument("-a", action="store_true", help="Download audio only.")
+    
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("-u", action="store_true", help="Download video.")
+    group.add_argument("-p", action="store_true", help="Download a playlist.")
+    group.add_argument("-c", action="store_true", help="Download all videos from a channel.")
+    
+    args = parser.parse_args()
+    
+    try:
+        Path(DOWNLOAD_PATH).mkdir(parents=True)
+    except FileExistsError:
+        print("Directory already exists")
+    
+    if DOWNLOAD_PATH == "Downloads\YouTube\\":
+        print(f"Videos will be downloaded to default path {DOWNLOAD_PATH}.")
+        
+    if args.u:
+        download_video(args.url, path=DOWNLOAD_PATH, audio_only=args.a)
+    elif args.p:
+        download_playlist(args.url, path=DOWNLOAD_PATH, audio_only=args.a)
+    elif args.c:
+        download_channel(args.url, path=DOWNLOAD_PATH, audio_only=args.a)
+    else:
+        print("Invalid combination of arguments.")
+        
+if __name__ == "__main__":  
+    main()
---
+++ utils.py
@@ -0,0 +1,57 @@
+from pytubefix import YouTube, Playlist, Channel
+from pytubefix.cli import on_progress
+
+
+def _download_streams(yt, path: str, audio_only: bool) -> None:
+    if audio_only:
+        ys = yt.streams
+        ys = ys.get_audio_only()
+        ys.download(output_path=path, mp3=True)
+    else:
+        ys = yt.streams
+        ys = ys.get_highest_resolution()
+        ys.download(output_path=path)
+         
+def download_video(url: str, path: str, audio_only: bool = False) -> None:
+    try:
+        yt = YouTube(url, on_progress_callback=on_progress)
+        print(yt.title)
+        _download_streams(yt, path, audio_only)
+    except Exception as e:
+        print(f"Error downloading video: {e}")
+
+def download_playlist(url: str, path: str, audio_only: bool = False) -> None:
+    try:
+        pl = Playlist(url)
+        print(f'Playlist name: {pl.title}')
+        
+        video_count = len(pl.videos)
+        print(f'Downloading... {video_count} videos found')
+        
+        for video in pl.videos:
+            print(f'{video.title}, left: {video_count}')
+            try:
+                _download_streams(video, path, audio_only)
+            except Exception as e:
+                print(f"Error downloading video {video.title}: {e}")
+            video_count -= 1
+    except Exception as e:
+        print(f"Error processing playlist: {e}")
+
+def download_channel(url: str, path: str, audio_only: bool = False) -> None:
+    try:
+        ch = Channel(url)
+        print(f'Channel name: {ch.channel_name}')
+        
+        video_count = len(ch.videos)
+        print(f'Downloading... {video_count} videos found')
+        
+        for video in ch.videos:
+            print(f'{video.title}, left: {video_count}')
+            try:
+                _download_streams(video, path, audio_only)
+            except Exception as e:
+                print(f"Error downloading video {video.title}: {e}")
+            video_count -= 1
+    except Exception as e:
+        print(f"Error processing channel: {e}")
